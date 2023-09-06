404e56b2b815f93dd3dd8cbfbf5ec518adb6bf72
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-09-06 13:19:35 -0500

    retrieved metadata

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 import axios from "axios";
+import getMetaData from './image';
 
 function App() {
 
   const [file, setFile] = useState();
   const [imageInput, setImageInput] = useState("");
+  const [mode,setMode] = useState("get image")
 
   function handleChange(evt) {
     setImageInput(evt.target.value);
     setFile(evt.target.files[0]);
   }
+
+ async function showPreview() {
+  setMode("preview")
+ }
+
 
   async function handleSubmit(evt) {
     evt.preventDefault();
@@ -18,9 +25,9 @@ function App() {
       file:file,
       fileName:file.name
     }
-    
+
     console.log("formData>>>>>>>>", file);
-    await axios.post("http://localhost:5000/add", formData, {
+    await axios.post("http://localhost:5001/add", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       }
@@ -29,7 +36,9 @@ function App() {
   }
 
   return (
-    <form onSubmit={handleSubmit} >
+
+    mode === "get image" ?
+    (<form onSubmit={showPreview} >
       <label htmlFor='image'> File Input </label>
       <input
         value={imageInput}
@@ -38,7 +47,15 @@ function App() {
         onChange={handleChange}
       />
       <button type="submit">Submit</button>
+    </form>) :
+
+    <div>
+      <img src={URL.createObjectURL(file)} onLoad={getMetaData}/>
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
     </form>
+    </div>
+
   );
 }
 
