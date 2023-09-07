2a51fe56e6439d6a5c92c91ec7a0564e651af084
Coco-ha <135057744+Coco-ha@users.noreply.github.com>
2023-09-07 12:41:17 -0500

    after thursday morning

--- src/AddImageForm.js
+++ src/AddImageForm.js
@@ -14,57 +14,70 @@ import ImagePreview from './ImagePreview';
  * PROPS:
  *  addImage: callback function for updating app state
 */
-function AddImageForm({addImage}) {
+function AddImageForm({ addImage }) {
 
   const [fileData, setFileData] = useState({});
   const [imageInput, setImageInput] = useState("");
   const [mode, setMode] = useState("select");
 
-  console.log("rendering addImageForm with:",fileData);
+  console.log("rendering addImageForm with:", fileData);
 
   /** Handles form controls */
   function handleChange(evt) {
-    setImageInput(evt.target.value);
-    setFileData(() => {
-      return { ...fileData, file: evt.target.files[0] };
-    });
+    const fileName = evt.target.files[0].name;
+    // console.log("targetfile>>>",evt.target.files[0])
+    // console.log("fileName>>>>>",fileName)
+    const splitFileName = fileName.toLowerCase().split(".")
+    const extension = splitFileName[splitFileName.length-1]
+
+    if (extension === "jpg") {
+      setImageInput(evt.target.value);
+      setFileData(() => {
+        return { ...fileData, file: evt.target.files[0] };
+      });
+    }else{
+      alert("This is not a JPG")
+      setImageInput("")
+    }
   }
 
   /** Accesses the file from the form input and updates fileData state.
    * Advances to preview mode.
   */
   async function goPreviewMode(evt) {
-    evt.preventDefault()
-    setMode("preview")
+    evt.preventDefault();
+    setMode("preview");
   }
 
   /** Updates the fileData state with metadata from an image.  Used as
    * a callback
    */
-  function updateMetadata(data){
-    console.log("passed data to updateMetadata", data)
+  function updateMetadata(data) {
+    console.log("passed data to updateMetadata", data);
     setFileData(() => {
       return {
         ...fileData,
         ...data,
       };
-    })
+    });
   }
 
   /** Makes an api request for uploading an image */
   async function handleUpload(evt) {
     evt.preventDefault();
-    console.log("running handle upload")
-    console.log("fileDATA>>>>", fileData)
-    await uploadImage(fileData)
+
+    console.log("running handle upload");
+    console.log("fileDATA>>>>", fileData);
+    await uploadImage(fileData);
     //TODO: redirect somewhere
+
   }
 
 
   return (
     <div className='AddImageForm'>
-    {mode === "select"
-      ?
+      {mode === "select"
+        ?
         <form onSubmit={goPreviewMode}>
           <label htmlFor='image'> File Input </label>
           <input
@@ -76,13 +89,13 @@ function AddImageForm({addImage}) {
           />
           <button type="submit">Submit</button>
         </form>
-      :
+        :
         <ImagePreview
           updateMetadata={updateMetadata}
           file={fileData.file}
-          save={handleUpload}/>
+          save={handleUpload} />
       }
-      </div>
+    </div>
   );
 }
 
